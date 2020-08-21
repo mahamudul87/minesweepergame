35f243a63d90db0a7ae2cc1ae55b4b572951924d
Md. Mahamudul Hasan <mahamudul.duet87@gmail.com>
2020-08-21 13:17:08 +0900

    Update Command.java

--- src/MineSweeper/Command.java
+++ src/MineSweeper/Command.java
@@ -1,11 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package MineSweeper;
 /**
- * @author ianw
+ * @author Hasan
  * A representation of what the user wants the game to do next.   
  */
 public class Command {
